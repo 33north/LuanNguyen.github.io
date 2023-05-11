e3e3a55125aca474ce4e6da0362acf7e1734ef5d
Luan Nguyen <luandacnguyen@protonmail.com>
2023-05-11 15:32:14 -0700

    20230511 - Add resume function to call resume.html page

--- app.py
+++ app.py
@@ -29,5 +29,11 @@ def about():
     """
     return render_template("about.html")
 
+@app.route("/resume/")
+def resume():
+    """
+    """
+    return render_template("resume.html")
+
 if __name__ == '__main__':
     app.run(host = "127.0.0.1", port = 5000, debug = True)
